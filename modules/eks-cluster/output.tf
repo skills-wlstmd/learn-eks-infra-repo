c6b2176ee754605fc6f845043b681207efae707f
wlstmd <jinwoo8288@gmail.com>
2024-08-04 13:05:36 +0900

    fix: trouble

--- modules/eks-cluster/output.tf
+++ modules/eks-cluster/output.tf
@@ -1,7 +1,15 @@
 output "cluster_id" {
-  value = module.eks.cluster_id
+  value = module.eks.cluster_name
 }
 
 output "cluster_primary_security_group_id" {
   value = module.eks.cluster_primary_security_group_id
+}
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  value = module.eks.cluster_certificate_authority_data
 }
--- modules/eks-cluster/variables.tf
+++ modules/eks-cluster/variables.tf
@@ -1,16 +1,30 @@
-
 variable "cluster_name" {
+  type = string
 }
+
 variable "cluster_version" {
+  type = string
 }
+
 variable "vpc_id" {
+  type = string
 }
+
 variable "public_subnets" {
+  type = list(string)
 }
+
 variable "private_subnets" {
+  type = list(string)
 }
+
 variable "create_access_entry" {
   description = "Whether to create the access entry"
   type        = bool
   default     = false
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
 }
